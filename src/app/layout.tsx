f5cbc424b28cbde6b7c472df0890fcfec6cae4e5
Haikal <kokhaikalberak12@gmail.com>
2026-04-01 15:04:59 +0700

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
+export const metadata: Metadata = {
+  title: "CollabSpace",
+  description:
+    "CollabSpace is Collaboration App That Simplify Your Collaboration With your Team",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn("h-full", "antialiased", "font-sans", inter.variable)}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
